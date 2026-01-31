f6da5f36ebfeb52ac2157b42a080ddc8d36731fe
ranajaafarmerhi <ranajaafarmerhi@gmail.com>
2026-01-31 12:10:45 +0200

    Implement ShopService

--- src/main/java/com/example/demo/service/ShopService.java
+++ src/main/java/com/example/demo/service/ShopService.java
@@ -1,12 +1,17 @@
+
 package com.example.demo.service;
 
-
-import  com.example.demo.entity.Shop;
+import com.example.demo.entity.Shop;
 import java.util.List;
 
 public interface ShopService {
 
-    Shop createShop(Shop shop);
-
     List<Shop> getAllShops();
+
+    List<Shop> getShopsByCategory(String category);
+
+    List<Shop> getShopsByLocation(String location);
+
+    Shop getShopById(Long id);
 }
+
